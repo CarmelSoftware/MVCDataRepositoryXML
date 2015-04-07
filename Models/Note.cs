f2149aa353c09989c14a74912093944c2188f46b
Carmel Shvartzman <carmel.aspnet@gmail.com>
2015-04-07 23:17:43 +0300

    Update Note.cs

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,1 +1,8 @@
-///  Note.cs
+public class Note
+{
+public int ID { get; set; }
+public string To { get; set; }
+public string From { get; set; }
+public string Heading { get; set; }
+public string Body { get; set; }
+}
