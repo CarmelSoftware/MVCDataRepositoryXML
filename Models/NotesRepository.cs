216903b1f665d2d8d183c2a09ed91e3a7edb6a5b
Carmel Shvartzman <carmel.aspnet@gmail.com>
2015-04-16 21:35:40 +0300

    Update NotesRepository.cs

--- Models/NotesRepository.cs
+++ Models/NotesRepository.cs
@@ -13,3 +13,19 @@ private int iNumberOfEntries = 1;
 private XDocument doc;
 
 public NotesRepository()
+{
+doc = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/data.xml"));
+foreach (var node in doc.Descendants("note"))
+{
+notes.Add(new Note
+{
+ID = Int32.Parse(node.Descendants("id").FirstOrDefault().Value),
+To = node.Descendants("to").FirstOrDefault().Value,
+From = node.Descendants("from").FirstOrDefault().Value,
+Heading = node.Descendants("heading").FirstOrDefault().Value,
+Body = node.Descendants("body").FirstOrDefault().Value
+});
+}
+
+iNumberOfEntries = notes.Count;
+}
